abf1626a564a9b432918974063477d280e5fe982
Muhammad Kaleemullah <mku.official.pk@gmail.com>
2026-05-19 12:36:17 +0500

    feat(backend): configure CORS and cookie-parser for cross-origin auth

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,6 +5,8 @@ const express = require("express"); //1st part
 const cookieParser = require("cookie-parser"); //6th part (after 8th part in auth.controller.js file) it is middleware
 const authRoutes = require("./routes/auth.routes"); //8th part
 const foodRputes = require("./routes/food.routes");
+const foodPartnerRoutes = require("./routes/food-partner.routes");
+const cors = require("cors"); //10th part (after 9th part in auth.controller.js file) it is middleware
 
 //creating server instance
 const app = express(); //2nd part
@@ -12,6 +14,12 @@ const app = express(); //2nd part
 app.use(cookieParser()); //7th part (middleware)
 app.use(express.json()); // 5th part (after 2nd part in auth.controller.js file) --> it is middleware
 //creating dummy route here
+
+app.use(cors({
+  origin: "http://localhost:5173", // yahan par hama apne frontend ka url dena ha, taki hamara backend us url sa aane wali request ko accept kar sake
+  credentials: true, // yahan par hama credentials ko true karna ha taki hamara backend cookies ko accept kar sake
+}))
+
 app.get("/", (req, res) => {
   // 4th part
   res.send("Hello World");
@@ -23,6 +31,8 @@ app.use("/api/auth", authRoutes); //9th part
 // --> /api/auth this is called prefix, yani auth ki koi bhi api ko hama acces karna ha to us sa pahla hama ya prefix lagana hoga
 
 app.use("/api/food", foodRputes);
+
+app.use("/api/food-partner", foodPartnerRoutes);
 
 //as we know here we only create server, we start server in the server.js file ==> so we have to
 //export this file to server.js
